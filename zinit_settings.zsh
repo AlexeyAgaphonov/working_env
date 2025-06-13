ee0bb9b713f3bf8551e299fe86764c580614c067
Alexey Agaphonov <alexeyagaphonov@gmail.com>
2025-06-13 12:05:05 +0300

    zsh file for preparing zinit

---
+++ zinit_settings.zsh
@@ -0,0 +1,17 @@
+# __BEGIN_ZINIT__
+ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.config/zsh/zinit/zinit.git}"
+[ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
+[ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+source "${ZINIT_HOME}/zinit.zsh"
+zinit ice depth=1; zinit light romkatv/powerlevel10k
+zinit light zsh-users/zsh-syntax-highlighting
+zinit light zsh-users/zsh-autosuggestions
+zinit light Aloxaf/fzf-tab
+autoload -U compinit && compinit
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+zstyle ':completion:*' menu no
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+eval "$(fzf --zsh)"
+# __END_ZINIT__
+
